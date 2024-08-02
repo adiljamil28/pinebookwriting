54e5ea2562318594a403d07e75015e1e762308a7
PineBookWriting2024 <info@pinebookwriting.com>
2024-08-02 05:10:29 +0500

    add file

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['imperiumdesigners.com', 'https://videos.ctfassets.net/0g2c0d4v74kt/7JddRckDmiDTFoj2zkuibz/19a06bc6173d8b205ead99ba8b0a0307/'],
+  },
+};
+
+export default nextConfig;
